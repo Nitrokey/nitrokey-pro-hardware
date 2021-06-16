2435a7834876e8b539a125480b73c367f577f49a
Kliment <kliment@0xfb.com>
2021-06-16 21:21:26 +0200

    Update panel alignment marks to match previous panel

--- panel/panel.py
+++ panel/panel.py
@@ -15,19 +15,19 @@ for i in range(25):
             rotationAngle=fromDegrees(0), shrink=False, tolerance=fromMm(25), bufferOutline=1000, 
             netRenamer=None, refRenamer=None)
 
-    
-panel.appendSubstrate(wxRectMM(0, 0, 10, 426.875))
-panel.appendSubstrate(wxRectMM(0, 0, 107.1, 9.7))
-panel.appendSubstrate(wxRectMM(97.1, 0, 10, 426.875))
-panel.appendSubstrate(wxRectMM(0, 416.875+0.3, 107.1, 9.7))
+panel.appendBoard("./panel-frame.kicad_pcb",wxPointMM(107.1/2,426.875/2),tolerance=fromMm(25))
+#panel.appendSubstrate(wxRectMM(0, 0, 10, 426.875))
+#panel.appendSubstrate(wxRectMM(0, 0, 107.1, 9.7))
+#panel.appendSubstrate(wxRectMM(97.1, 0, 10, 426.875))
+#panel.appendSubstrate(wxRectMM(0, 416.875+0.3, 107.1, 9.7))
 for i in range(26):
     panel.addVCutH(fromMm(10+i*16.275))
 for i in range(5):
     panel.addVCutV(fromMm(10+i*21.775))
 #panel.copperFillNonBoardAreas()
-panel.addNPTHole(wxPointMM(94.08, 2.575),fromMm(2.05))
-panel.addNPTHole(wxPointMM(13, 424.325),fromMm(2.05))
-panel.addFiducials(fromMm(8), fromMm(4), fromMm(0.5), fromMm(1))
+#panel.addNPTHole(wxPointMM(94.08, 2.575),fromMm(2.05))
+#panel.addNPTHole(wxPointMM(13, 424.325),fromMm(2.05))
+#panel.addFiducials(fromMm(8), fromMm(4), fromMm(0.5), fromMm(1))
 panel.addTooling(fromMm(4), fromMm(4), fromMm(2))
 panel.addMillFillets(fromMm(0.4))
 panel.save("out.kicad_pcb")
